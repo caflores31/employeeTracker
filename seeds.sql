13fa71e1afb5db01bc879bc070b039d205810be0
nosremetnarg <grant.alex.emerson@gmail.com>
2020-08-20 08:44:46 -0500

    install npm inquirer jest

---
+++ seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (name)
+VALUES
+('BACKEND DEV', 'We handle the data so you don"t have to'),
+('FRONTEND DEV', 'Custmer UX is our expertice'),
+('MANAGER', 'We keep our team on target');
+
+INSERT INTO role_employee (title, salary, department_id)
+VALUES
+('')
