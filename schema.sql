30a2c674803fa90f22d17a81fb0460ae517feccb
nosremetnarg <grant.alex.emerson@gmail.com>
2020-08-20 08:25:54 -0500

    database creation

--- schema.sql
+++ schema.sql
@@ -2,6 +2,11 @@ DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role_employee;
 DROP TABLE IF EXISTS employee;
 
+-- database creation
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+USE employeeDB;
+
 CREATE TABLE department (
     id INTEGER PRIMARY KEY,
     name VARCHAR(30) NOT NULL
